d7e6a1edd55c28016774eec86e6054f4995d18be
MaxSchwarzer <maxon999@comcast.net>
2019-04-13 22:27:19 -0400

    Initial commit with GAN code, code for problem 1

---
+++ assignment3/discrim.py
@@ -0,0 +1,268 @@
+import torch as T
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from samplers import distribution1, distribution2, distribution3, distribution4
+if T.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+log2 = np.log(2)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, inchannels, outchannels, stride=2):
+        super(ResBlock, self).__init__()
+        if inchannels != outchannels:
+            if stride > 0:
+                self.upscaler = nn.utils.spectral_norm(nn.Conv2d(inchannels, outchannels, 3,
+                                                                 stride=stride, padding=1))
+            elif stride < 0:
+                self.upscaler = nn.utils.spectral_norm(nn.ConvTranspose2d(inchannels, outchannels, 4,
+                                                                          padding=1, stride=-stride))
+            self.upscale = True
+        else:
+            self.upscale = False
+
+        self.conv1 = nn.utils.spectral_norm(nn.Conv2d(outchannels, outchannels, 3, padding=1))
+        self.bn1 = nn.BatchNorm2d(outchannels)
+        self.conv2 = nn.utils.spectral_norm(nn.Conv2d(outchannels, outchannels, 3, padding=1))
+        self.bn2 = nn.BatchNorm2d(outchannels)
+
+        self.relu = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        if self.upscale:
+            old_x = x
+            x = self.upscaler(x)
+        x1 = self.conv1(x)
+        x1 = self.bn1(self.relu(x1))
+        x2 = self.conv2(x1)
+        x2 = self.bn2(self.relu(x2))
+
+        output = x2 + x
+        return output
+
+
+class Generator(nn.Module):
+    def __init__(self, zdim, hdim, im_channels=3, blocks=[2, 2, 2, 2]):
+        super(Generator, self).__init__()
+
+        self.initial = nn.Linear(zdim, 4*hdim)
+
+        self.blocks = nn.ModuleList()
+
+        current_dim = hdim
+        new_dim = hdim
+        for nblocks in blocks:
+            for block in range(nblocks):
+                if new_dim != current_dim:
+                    self.blocks.append(ResBlock(current_dim, new_dim, stride=-2))
+                    current_dim = new_dim
+                self.blocks.append(ResBlock(current_dim, current_dim, stride=1))
+            new_dim = current_dim//2
+
+        self.final = nn.utils.spectral_norm(nn.Conv2d(current_dim, im_channels, 7, padding=3))
+
+    def forward(self, x):
+        current = self.initial(x)
+        current = current.view(x.shape[0], -1, 2, 2)
+
+        for block in self.blocks:
+            current = block(current)
+
+        output = T.sigmoid(self.final(current))
+
+        return output
+
+
+class ConvDiscriminator(nn.Module):
+    def __init__(self, hdim, im_channels=3, blocks=[2, 2, 2, 2]):
+        super(ConvDiscriminator, self).__init__()
+        self.blocks = nn.ModuleList()
+
+        self.initial = nn.Conv2d(im_channels, hdim, 7, padding=3)
+        self.initial_bn = nn.BatchNorm2d(hdim)
+        self.relu = nn.LeakyReLU(0.2)
+
+        current_dim = hdim
+        new_dim = current_dim
+        for nblocks in blocks:
+            for block in range(nblocks):
+                if new_dim != current_dim:
+                    self.blocks.append(ResBlock(current_dim, new_dim, stride=2))
+                    current_dim = new_dim
+                self.blocks.append(ResBlock(current_dim, current_dim, stride=1))
+            new_dim = current_dim*2
+
+        self.fc1 = nn.utils.spectral_norm(nn.Linear(current_dim*4, current_dim))
+        self.fc2 = nn.utils.spectral_norm(nn.Linear(current_dim, 1))
+
+    def forward(self, x):
+        current = self.initial(x)
+        current = self.initial_bn(self.relu(current))
+
+        for block in self.blocks:
+            current = block(current)
+
+        current = self.fc1(current.flatten(1, -1))
+        current = self.relu(current)
+        return self.fc2(current)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, indim, hdim):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(indim, hdim)
+        self.relu_1 = nn.ReLU()
+        self.fc2 = nn.Linear(hdim, hdim)
+        self.relu_2 = nn.ReLU()
+        self.fc3 = nn.Linear(hdim, 1)
+
+        nn.init.kaiming_uniform_(self.fc1.weight)
+        nn.init.kaiming_uniform_(self.fc2.weight)
+        nn.init.kaiming_uniform_(self.fc3.weight)
+
+    def forward(self, x):
+        x1 = self.fc1(x)
+        x1 = self.relu_1(x1)
+
+        x2 = self.fc2(x1)
+        x2 = self.relu_2(x2)
+        x3 = self.fc3(x2)
+
+        return x3
+
+
+def wgan_objective(model, real, fake, lda=10):
+    d_real = model(real)
+    d_fake = model(fake)
+
+    alpha = T.rand((real.shape[0]), device=real.device)
+    for i in range(len(real.shape) - 1):
+        alpha.unsqueeze_(-1)
+    interps = alpha*real + (1 - alpha)*fake.detach()
+    interps.requires_grad = True
+
+    output = model(interps).squeeze(-1)
+
+    grad = T.autograd.grad(output, interps,
+                           retain_graph=True, create_graph=True,
+                           grad_outputs=T.ones(output.shape[0], device=device))[0]
+    grad_penalty = (T.norm(grad, p=2, dim=-1) - 1)**2
+
+    # print(grad_penalty.mean().item(), d_real.mean().item(), d_fake.mean().item())
+    d_loss = d_fake.mean() - d_real.mean() + lda*grad_penalty.mean()
+    g_loss = -d_fake.mean()
+
+    return d_loss, g_loss, d_real.mean() - d_fake.mean()
+
+
+def jsd_objective(model, real, fake):
+    d_real = model(real).squeeze(-1)
+    d_fake = model(fake).squeeze(-1)
+
+    l_real = F.logsigmoid(d_real)
+    l_fake = F.logsigmoid(1 - d_fake)
+
+    # don't include the log2 term, as it's constant wrt parameters
+    loss = 0.5*(l_real + l_fake)
+
+    # if T.sigmoid(d_fake).mean() > 0.6:
+    #     import ipdb;
+    #     ipdb.set_trace()
+    # print(T.sigmoid(d_real).mean().item(), T.sigmoid(d_fake).mean().item())
+    d_loss = -loss.mean()
+    g_loss = loss.mean()
+    real_jsd = loss.mean() + log2
+
+    return d_loss, g_loss, real_jsd.mean()
+
+
+def problem3(bs=512, iters=1000):
+    divs = []
+    for loss_fcn in [jsd_objective, wgan_objective]:
+        divs.append([])
+        for theta in np.linspace(1, -1, 21, endpoint=True):
+            network = Discriminator(2, 256)
+            network = network.to(device)
+            optimizer = optim.SGD(network.parameters(), 1e-2, 0.9)
+            i = 0
+            for real, fake in zip(distribution1(0, bs), distribution1(theta, bs)):
+                i += 1
+                real = T.tensor(real, device=device).float()
+                fake = T.tensor(fake, device=device).float()
+
+                dl, gl, div = loss_fcn(network, real, fake)
+                dl.sum().backward()
+                nn.utils.clip_grad_norm_(network.parameters(), 1)
+                optimizer.step()
+                optimizer.zero_grad()
+                if i > iters:
+                    break
+                elif i % 99 == 0:
+                    print(div.item())
+
+            divs[-1].append(div.detach().cpu().numpy())
+
+    plt.figure()
+    plt.scatter(np.linspace(-1, 1, 21, endpoint=True), divs[0], label="JSD")
+    plt.legend()
+    plt.xlabel("Theta")
+    plt.ylabel("Estimated divergence")
+    plt.title("Estimated JSD for disjoint distributions")
+    plt.savefig("3-1.pdf")
+
+    plt.figure()
+    plt.scatter(np.linspace(-1, 1, 21, endpoint=True), divs[1], label="Wasserstein Estimate")
+    plt.legend()
+    plt.xlabel("Theta")
+    plt.ylabel("Estimated divergence")
+    plt.title("Estimated Wasserstein distance for disjoint distributions")
+    plt.savefig("3-2.pdf")
+
+
+def problem4(bs=512, iters=10000):
+    loss_fcn = jsd_objective
+    normal = T.distributions.normal.Normal(0, 1)
+    network = Discriminator(1, 128)
+    network = network.to(device)
+    optimizer = optim.SGD(network.parameters(), 1e-3, 0.9)
+    i = 0
+    for real, fake in zip(distribution4(bs), distribution3(bs)):
+        i += 1
+        real = T.tensor(real, device=device).float()
+        fake = T.tensor(fake, device=device).float()
+        loss, gen_loss, jsd = loss_fcn(network, real, fake)
+        loss.backward()
+        nn.utils.clip_grad_norm_(network.parameters(), 1)
+        optimizer.step()
+        optimizer.zero_grad()
+        if i > iters:
+            break
+        elif i % 100 == 0:
+            print(loss.item(), jsd.item())
+
+    probe = np.linspace(-10, 10, 10000)
+    probe = T.tensor(probe, device=device).float().unsqueeze(-1)
+
+    density = T.exp(normal.log_prob(probe)).squeeze(-1).detach().cpu().numpy()
+    dx = T.sigmoid(network(probe)).squeeze(-1).detach().cpu().numpy()
+    density_estimate = dx*density/(1 - dx + 0.0001)
+
+    plt.figure()
+    plt.plot(probe.cpu().numpy()[:, 0], density_estimate, label="Estimated density")
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("Estimated density p(x)")
+    plt.title("Estimated pdf of Distribution 4")
+    plt.savefig("4-1.pdf")
+
+
+if __name__ == "__main__":
+    problem3(512)
+    problem4(512)
+
+
---
+++ assignment3/gan_training.py
@@ -0,0 +1,158 @@
+import torch as T
+from torch import nn
+from torch import optim
+from torch import utils
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import argparse
+import time
+from samplers import distribution1, distribution2, distribution3, distribution4
+if T.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+
+from discrim import wgan_objective, Generator, ConvDiscriminator
+
+class GAN(nn.Module):
+    def __init__(self, zdim=100, channels=32, blocks=[1, 1, 1, 1, 1]):
+        super(GAN, self).__init__()
+        self.generator = Generator(zdim=zdim, im_channels=3, hdim=channels*2**(len(blocks)-1), blocks=blocks)
+        self.discriminator = ConvDiscriminator(im_channels=3, hdim=channels, blocks=blocks,)
+        self.distribution = T.distributions.normal.Normal(0, 1)
+        self.d_optim = optim.Adam(self.discriminator.parameters(), 4e-4, betas=(0.5, 0.9))
+        self.g_optim = optim.Adam(self.generator.parameters(), 1e-4, betas=(0.5, 0.9))
+
+        self.zdim = zdim
+
+    def sample(self, batch_size):
+        sample_z = self.distribution.sample((batch_size, self.zdim)).to(device)
+        sample_images = self.generator(sample_z)
+        return sample_images
+
+    def sample_numpy(self, batch_size=32, total=1000, save="GAN_Samples"):
+        with T.no_grad():
+            images = []
+            for batch in range(np.ceil(batch_size/total)):
+                sample_z = self.distribution.sample((batch_size, self.zdim)).to(device)
+                sample_images = self.generator(sample_z)
+                images.append(sample_images)
+
+        images = np.concatenate(images.detach().cpu().numpy(), 0)[:total]
+        if save is not None:
+            np.savez(save, images=images)
+
+
+        return images
+
+    def perturb_sample(self, save="perturbed_images"):
+        with T.no_grad():
+            sample_z = self.distribution.sample((1, self.zdim)).to(device)
+            image_original = self.generator(sample_z)
+            images = [image_original]
+
+            for i in range(self.zdim):
+                perturbed = T.copy(sample_z)
+                perturbed[:, i] + 0.1
+                images.append(self.generator(perturbed))
+
+            images = [im.detach().cpu().numpy() for im in images]
+            images = np.concatenate(images, 0)
+            torchvision.utils.save_image(images)
+            np.savez(save, images=images)
+
+
+    def interpolate(self, save="interpolations"):
+        with T.no_grad():
+            alphas = np.linspace(0, 1, 11, endpoint=True)
+            sample_1 = self.distribution.sample((1, self.zdim)).to(device)
+            sample_2 = self.distribution.sample((1, self.zdim)).to(device)
+            image_1 = self.generator(sample_1)
+            image_2 = self.generator(sample_2)
+
+            im_interps = []
+            for alpha in alphas:
+                im_interps.append(image_1*alpha + image_2*(1 - alpha))
+
+            latent_interps = []
+            for alpha in alphas:
+                latent_interps.append(self.generator(sample_1*alpha + sample_2*(1 - alpha)))
+
+            im_interps = [im.detach().cpu().numpy() for im in im_interps]
+            latent_interps = [im.detach().cpu().numpy() for im in latent_interps]
+
+        np.savez(save, im_interps=im_interps, latent_interps=latent_interps)
+
+
+def train(flags):
+    dataset = torchvision.datasets.SVHN("./svhn/",
+                                        split='train',
+                                        transform=torchvision.transforms.ToTensor(),
+                                        target_transform=None,
+                                        download=True)
+    loader = utils.data.DataLoader(dataset, batch_size=flags.batch_size, shuffle=True)
+    if flags.load is not None:
+        model = T.load(flags.load)
+    else:
+        model = GAN(flags.zdim, flags.channels)
+    model = model.to(device)
+
+    old_time = time.clock()
+    for i, (real, _) in enumerate(loader):
+        samples = model.sample(flags.batch_size)
+        real = real.to(device)
+        d_loss, g_loss, div = wgan_objective(model.discriminator, real, samples)
+
+        d_loss.backward(retain_graph=True)
+        nn.utils.clip_grad_norm_(model.discriminator.parameters(), 1)
+        model.d_optim.step()
+        model.d_optim.zero_grad()
+        g_loss.backward()
+        nn.utils.clip_grad_norm_(model.generator.parameters(), 1)
+        model.g_optim.step()
+        model.g_optim.zero_grad()
+
+        if i > flags.iters:
+            break
+        if i % flags.print_freq == 0:
+            current_time = time.clock()
+            per_iter = (current_time - old_time)/flags.print_freq
+            old_time = current_time
+            print(d_loss.item(), g_loss.item(), div.item(), per_iter)
+
+        if i % flags.sample_freq == 0:
+            file = flags.save.replace(".pt", "") + str(i) + ".png"
+            torchvision.utils.save_image(samples[:16].detach().cpu(), file)
+        if i % flags.save_freq == 0:
+            T.save(model, flags.save)
+
+
+
+    return model
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='WGAN-GP training.')
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument("--iters", type=int, default=100000, help="Number of iters to train for")
+    parser.add_argument("--sample_freq", type=int, default=1000, help="How often to sample")
+    parser.add_argument("--print_freq", type=int, default=10, help="How often to print")
+    parser.add_argument("--save_freq", type=int, default=1000, help="How often to save")
+    parser.add_argument("--load", type=str, default=None, help="File to load model from.  Blank for none.")
+    parser.add_argument("--save", type=str, default="wgan.pt", help="File to save model to.")
+    parser.add_argument("--zdim", type=int, default=100, help="Dimension of latent")
+    parser.add_argument("--channels", type=int, default=8, help="Base # of channels in resnets.")
+
+    flags = parser.parse_args()
+    model = train(flags)
+    model.sample_numpy()
+    model.perturb_sample()
+    model.interpolate()
+
+
+
+
