a78eb4831e11ccefa86b7e824f729db23c92bbd9
MaxSchwarzer <maxon999@comcast.net>
2019-04-13 22:48:56 -0400

    Clean up printing

--- assignment3/gan_training.py
+++ assignment3/gan_training.py
@@ -126,7 +126,7 @@ def train(flags):
             current_time = time.clock()
             per_iter = (current_time - old_time)/flags.print_freq
             old_time = current_time
-            print("Generator gap: {0:.4f}; time per iter: {}".format(div.item(), per_iter))
+            print("Generator gap: {0:.4f}; time per iter: {1:.4f}".format(div.item(), per_iter))
 
         if i % flags.sample_freq == 0:
             file = flags.save.replace(".pt", "") + str(i) + ".png"
