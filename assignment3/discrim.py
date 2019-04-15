d4f653808ad03e1a989ab213c3c40e2a78ddf1cf
Max Schwarzer <schwarzm@leto52.server.mila.quebec>
2019-04-15 11:41:48 -0400

    Add working WGAN code

--- assignment3/discrim.py
+++ assignment3/discrim.py
@@ -13,96 +13,131 @@ log2 = np.log(2)
 
 
 class ResBlock(nn.Module):
-    def __init__(self, inchannels, outchannels, stride=2):
+    def __init__(self, inchannels, outchannels, stride=2, dropout=0.5, bn=False, ln=False, sn=False, h=None):
         super(ResBlock, self).__init__()
         if inchannels != outchannels:
             if stride > 0:
-                self.upscaler = nn.utils.spectral_norm(nn.Conv2d(inchannels, outchannels, 3,
+                self.upscaler = (nn.Conv2d(inchannels, outchannels, 3,
                                                                  stride=stride, padding=1))
+                if sn:
+                    self.upscaler = nn.utils.spectral_norm(self.upscaler)
             elif stride < 0:
-                self.upscaler = nn.utils.spectral_norm(nn.ConvTranspose2d(inchannels, outchannels, 4,
+                self.upscaler = (nn.ConvTranspose2d(inchannels, outchannels, 4,
                                                                           padding=1, stride=-stride))
+            self.relu0 = nn.LeakyReLU(0.2)
             self.upscale = True
         else:
             self.upscale = False
 
-        self.conv1 = nn.utils.spectral_norm(nn.Conv2d(outchannels, outchannels, 3, padding=1))
-        self.bn1 = nn.BatchNorm2d(outchannels)
-        self.conv2 = nn.utils.spectral_norm(nn.Conv2d(outchannels, outchannels, 3, padding=1))
-        self.bn2 = nn.BatchNorm2d(outchannels)
+        self.bn = bn or ln
 
-        self.relu = nn.LeakyReLU(0.2)
+        self.stride = -stride if stride < 0 else 1/stride
+        self.conv1 = (nn.Conv2d(outchannels, outchannels, 3, padding=1))
+        self.dropout1 = nn.Dropout(dropout)
+        if bn:
+            self.bn1 = nn.BatchNorm2d(outchannels)
+            self.bn2 = nn.BatchNorm2d(outchannels)
+        elif ln:
+            self.bn1 = nn.LayerNorm((outchannels, h, h))
+            self.bn2 = nn.LayerNorm((outchannels, h, h))
+        self.conv2 = (nn.Conv2d(outchannels, outchannels, 3, padding=1))
+        self.dropout2 = nn.Dropout(dropout)
+        if sn:
+            self.conv1 = nn.utils.spectral_norm(self.conv1)
+            self.conv2 = nn.utils.spectral_norm(self.conv2)
+
+        self.relu1 = nn.LeakyReLU(0.2)
+        self.relu2 = nn.LeakyReLU(0.2)
 
     def forward(self, x):
         if self.upscale:
-            old_x = x
+            # x = F.interpolate(x, scale_factor=self.stride)
             x = self.upscaler(x)
+            x = self.relu0(x)
+            if self.bn:
+                x = self.bn1(x)
         x1 = self.conv1(x)
-        x1 = self.bn1(self.relu(x1))
+        x1 = self.relu1(x1)
+        if self.bn:
+            x1 = self.bn1(x1)
+        x1 = self.dropout1(x1)
         x2 = self.conv2(x1)
-        x2 = self.bn2(self.relu(x2))
+        x2 = self.relu2(x2)
+        if self.bn:
+            x2 = self.bn2(x2)
+        x2 = self.dropout2(x2)
 
         output = x2 + x
         return output
 
 
 class Generator(nn.Module):
-    def __init__(self, zdim, hdim, im_channels=3, blocks=[2, 2, 2, 2]):
+    def __init__(self, zdim, hdim, im_channels=3, blocks=[2, 2, 2, 2], dropout=0.5):
         super(Generator, self).__init__()
 
-        self.initial = nn.Linear(zdim, 4*hdim)
+        self.initial = nn.Linear(zdim, 16*hdim)
 
         self.blocks = nn.ModuleList()
 
         current_dim = hdim
         new_dim = hdim
+        h = 4
         for nblocks in blocks:
             for block in range(nblocks):
                 if new_dim != current_dim:
-                    self.blocks.append(ResBlock(current_dim, new_dim, stride=-2))
+                    self.blocks.append(ResBlock(current_dim, new_dim, stride=-2, dropout=dropout, bn=True, sn=False, h=h))
                     current_dim = new_dim
-                self.blocks.append(ResBlock(current_dim, current_dim, stride=1))
+                self.blocks.append(ResBlock(current_dim, current_dim, stride=1, dropout=dropout, bn=True, sn=False, h=h))
             new_dim = current_dim//2
+            h *= 2
 
-        self.final = nn.utils.spectral_norm(nn.Conv2d(current_dim, im_channels, 7, padding=3))
+        self.final = (nn.Conv2d(current_dim, im_channels, 7, padding=3))
+        self.final_bn = nn.BatchNorm2d(im_channels)
 
     def forward(self, x):
         current = self.initial(x)
-        current = current.view(x.shape[0], -1, 2, 2)
+        current = current.view(x.shape[0], -1, 4, 4)
 
         for block in self.blocks:
             current = block(current)
 
+        # output = 0.5*self.final_bn(T.sigmoid(self.final(current))) + 0.5
+        # output = T.sigmoid(self.final_bn(self.final(current)))
         output = T.sigmoid(self.final(current))
 
         return output
 
 
 class ConvDiscriminator(nn.Module):
-    def __init__(self, hdim, im_channels=3, blocks=[2, 2, 2, 2]):
+    def __init__(self, hdim, im_channels=3, blocks=[2, 2, 2, 2], dropout=0):
         super(ConvDiscriminator, self).__init__()
         self.blocks = nn.ModuleList()
 
+        self.im_bn = nn.BatchNorm2d(im_channels)
         self.initial = nn.Conv2d(im_channels, hdim, 7, padding=3)
         self.initial_bn = nn.BatchNorm2d(hdim)
         self.relu = nn.LeakyReLU(0.2)
 
+        h = 32
         current_dim = hdim
         new_dim = current_dim
         for nblocks in blocks:
             for block in range(nblocks):
                 if new_dim != current_dim:
-                    self.blocks.append(ResBlock(current_dim, new_dim, stride=2))
+                    self.blocks.append(ResBlock(current_dim, new_dim, stride=2, dropout=dropout, ln=True, sn=False, h=h))
                     current_dim = new_dim
-                self.blocks.append(ResBlock(current_dim, current_dim, stride=1))
+                self.blocks.append(ResBlock(current_dim, current_dim, stride=1, dropout=dropout, ln=True, sn=False, h=h))
             new_dim = current_dim*2
+            h = h//2
 
-        self.fc1 = nn.utils.spectral_norm(nn.Linear(current_dim*4, current_dim))
-        self.fc2 = nn.utils.spectral_norm(nn.Linear(current_dim, 1))
+        self.fc1 = (nn.Linear(current_dim*16, current_dim))
+        self.fc2 = (nn.Linear(current_dim, 1))
 
     def forward(self, x):
+        # x = self.im_bn(x)
         current = self.initial(x)
-        current = self.initial_bn(self.relu(current))
+        current = self.relu(current)
+        # current = self.initial_bn(current)
 
         for block in self.blocks:
             current = block(current)
@@ -136,14 +171,14 @@ class Discriminator(nn.Module):
         return x3
 
 
-def wgan_objective(model, real, fake, lda=10):
+def wgan_objective(model, real, fake, lda=5):
     d_real = model(real)
     d_fake = model(fake)
 
     alpha = T.rand((real.shape[0]), device=real.device)
     for i in range(len(real.shape) - 1):
         alpha.unsqueeze_(-1)
-    interps = alpha*real + (1 - alpha)*fake.detach()
+    interps = alpha*real.data + (1 - alpha)*fake.data
     interps.requires_grad = True
 
     output = model(interps).squeeze(-1)
@@ -151,7 +186,7 @@ def wgan_objective(model, real, fake, lda=10):
     grad = T.autograd.grad(output, interps,
                            retain_graph=True, create_graph=True,
                            grad_outputs=T.ones(output.shape[0], device=device))[0]
-    grad_penalty = (T.norm(grad, p=2, dim=-1) - 1)**2
+    grad_penalty = (T.norm(grad + 1e-16, dim=-1) - 1)**2
 
     # print(grad_penalty.mean().item(), d_real.mean().item(), d_fake.mean().item())
     d_loss = d_fake.mean() - d_real.mean() + lda*grad_penalty.mean()
--- assignment3/gan_training.py
+++ assignment3/gan_training.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
+import seaborn as sns
 import argparse
 import time
 from samplers import distribution1, distribution2, distribution3, distribution4
@@ -16,21 +17,83 @@ else:
 
 
 from discrim import wgan_objective, Generator, ConvDiscriminator
+# DIM = 128
+
+
+# class Generator(nn.Module):
+#     def __init__(self):
+#         super(Generator, self).__init__()
+#         preprocess = nn.Sequential(
+#             nn.Linear(100, 4 * 4 * 4 * DIM),
+#         )
+#
+#         block1 = nn.Sequential(
+#             nn.BatchNorm2d(4 * DIM),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(4 * DIM, 2 * DIM, 2, stride=2),
+#             nn.BatchNorm2d(2 * DIM),
+#             nn.ReLU(True),
+#         )
+#         block2 = nn.Sequential(
+#             nn.ConvTranspose2d(2 * DIM, DIM, 2, stride=2),
+#             nn.BatchNorm2d(DIM),
+#             nn.ReLU(True),
+#         )
+#         deconv_out = nn.ConvTranspose2d(DIM, 3, 2, stride=2)
+#
+#         self.preprocess = preprocess
+#         self.block1 = block1
+#         self.block2 = block2
+#         self.deconv_out = deconv_out
+#         self.sigmoid = nn.Sigmoid()
+#
+#     def forward(self, input):
+#         output = self.preprocess(input)
+#         output = output.view(-1, 4 * DIM, 4, 4)
+#         output = self.block1(output)
+#         output = self.block2(output)
+#         output = self.deconv_out(output)
+#         output = self.sigmoid(output)
+#         return output.view(-1, 3, 32, 32)
+#
+#
+# class Discriminator(nn.Module):
+#     def __init__(self):
+#         super(Discriminator, self).__init__()
+#         main = nn.Sequential(
+#             nn.Conv2d(3, DIM, 3, 2, padding=1),
+#             nn.LeakyReLU(),
+#             nn.Conv2d(DIM, 2 * DIM, 3, 2, padding=1),
+#             nn.LeakyReLU(),
+#             nn.Conv2d(2 * DIM, 4 * DIM, 3, 2, padding=1),
+#             nn.LeakyReLU(),
+#         )
+#
+#         self.main = main
+#         self.linear = nn.Linear(4*4*4*DIM, 1)
+#
+#     def forward(self, input):
+#         output = self.main(input)
+#         output = output.view(-1, 4*4*4*DIM)
+#         output = self.linear(output)
+#         return output
 
 class GAN(nn.Module):
-    def __init__(self, zdim=100, channels=32, blocks=[1, 1, 1, 1, 1]):
+    def __init__(self, zdim=100, channels_g=32, channels_d=32, blocks=[1, 1, 1, 1, 1]):
         super(GAN, self).__init__()
-        self.generator = Generator(zdim=zdim, im_channels=3, hdim=channels*2**(len(blocks)-1), blocks=blocks)
-        self.discriminator = ConvDiscriminator(im_channels=3, hdim=channels, blocks=blocks,)
+        self.generator = Generator(zdim=zdim, im_channels=3, hdim=channels_g*2**(len(blocks)-1), blocks=blocks)
+        self.discriminator = ConvDiscriminator(im_channels=3, hdim=channels_d, blocks=blocks,)
         self.distribution = T.distributions.normal.Normal(0, 1)
-        self.d_optim = optim.Adam(self.discriminator.parameters(), 1e-4, betas=(0.0, 0.9))
-        self.g_optim = optim.Adam(self.generator.parameters(), 1e-4, betas=(0.0, 0.9))
+        self.d_optim = optim.Adam(self.discriminator.parameters(), 4e-4, betas=(0.5, 0.9))
+        self.g_optim = optim.Adam(self.generator.parameters(), 1e-4, betas=(0.5, 0.9))
 
         self.zdim = zdim
 
     def sample(self, batch_size):
         sample_z = self.distribution.sample((batch_size, self.zdim)).to(device)
         sample_images = self.generator(sample_z)
+        print(T.var(sample_images.flatten(1, -1), 0).mean(), T.mean(sample_images.flatten(2, -1), -1).mean(0))
+
         return sample_images
 
     def sample_numpy(self, batch_size=32, total=1000, save="GAN_Samples"):
@@ -44,8 +107,6 @@ class GAN(nn.Module):
         images = np.concatenate(images.detach().cpu().numpy(), 0)[:total]
         if save is not None:
             np.savez(save, images=images)
-
-
         return images
 
     def perturb_sample(self, save="perturbed_images"):
@@ -63,7 +124,6 @@ class GAN(nn.Module):
             images = np.concatenate(images, 0)
             torchvision.utils.save_image(images)
             np.savez(save, images=images)
-
 
     def interpolate(self, save="interpolations"):
         with T.no_grad():
@@ -88,16 +148,17 @@ class GAN(nn.Module):
 
 
 def train(flags):
+    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
     dataset = torchvision.datasets.SVHN("./svhn/",
                                         split='train',
-                                        transform=torchvision.transforms.ToTensor(),
+                                        transform=transform,
                                         target_transform=None,
                                         download=True)
     loader = utils.data.DataLoader(dataset, batch_size=flags.batch_size, shuffle=True)
     if flags.load is not None:
         model = T.load(flags.load)
     else:
-        model = GAN(flags.zdim, flags.channels, blocks=[flags.blocks for i in range(5)])
+        model = GAN(flags.zdim, flags.channels_g, flags.channels_d, blocks=[flags.blocks for i in range(4)])
     model = model.to(device)
 
     old_time = time.clock()
@@ -112,12 +173,12 @@ def train(flags):
                 samples = model.sample(real.shape[0])
             real = real.to(device)
             d_loss, g_loss, div = wgan_objective(model.discriminator, real, samples)
-
             d_loss.backward(retain_graph=True)
             nn.utils.clip_grad_norm_(model.discriminator.parameters(), 1)
             model.d_optim.step()
             model.d_optim.zero_grad()
             if i % flags.disc_iters == 0:
+                model.g_optim.zero_grad()
                 g_loss.backward()
                 nn.utils.clip_grad_norm_(model.generator.parameters(), 1)
                 model.g_optim.step()
@@ -134,21 +195,31 @@ def train(flags):
             if i % flags.sample_freq == 0:
                 file = flags.save.replace(".pt", "") + str(i) + ".png"
                 print("Saving samples in {}".format(file))
-                torchvision.utils.save_image(samples[:16].detach().cpu(), file)
+                torchvision.utils.save_image(samples.detach().cpu(), file)
+                raw = samples.detach().cpu().flatten(2, -1).mean((-1)).numpy()
+                plt.figure()
+                sns.distplot(raw[:, 0])
+                sns.distplot(raw[:, 1])
+                sns.distplot(raw[:, 2])
+                plt.savefig(flags.save + "{}sample_dist.pdf".format(i))
+                plt.close('all')
+                raw = real.detach().cpu().flatten(2, -1).mean((-1)).numpy()
+                plt.figure()
+                sns.distplot(raw[:, 0])
+                sns.distplot(raw[:, 1])
+                sns.distplot(raw[:, 2])
+                plt.savefig(flags.save + "{}real_dist.pdf".format(i))
+                plt.close('all')
             if i % flags.save_freq == 0:
                 print("Saving model in {}".format(flags.save))
                 T.save(model, flags.save)
-
-
-
     return model
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='WGAN-GP training.')
     parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument("--iters", type=int, default=100000, help="Number of iters to train for")
+    parser.add_argument("--iters", type=int, default=2000000, help="Number of iters to train for")
     parser.add_argument("--disc_iters", type=int, default=4, help="Number of disc iters per gen iter")
     parser.add_argument("--sample_freq", type=int, default=1000, help="How often to sample")
     parser.add_argument("--print_freq", type=int, default=10, help="How often to print")
@@ -157,7 +228,8 @@ if __name__ == "__main__":
     parser.add_argument("--save", type=str, default="wgan.pt", help="File to save model to.")
     parser.add_argument("--zdim", type=int, default=100, help="Dimension of latent")
     parser.add_argument("--blocks", type=int, default=2, help="# of resblocks per pooling")
-    parser.add_argument("--channels", type=int, default=8, help="Base # of channels in resnets.")
+    parser.add_argument("--channels_g", type=int, default=32, help="Base # of channels in g resnets.")
+    parser.add_argument("--channels_d", type=int, default=32, help="Base # of channels in d resnets.")
 
     flags = parser.parse_args()
     model = train(flags)
