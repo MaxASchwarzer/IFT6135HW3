91d8dfa62135b6c883a6b943a453469ab05827f0
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2019-04-15 11:45:12 -0400

    Remove old code, add gitignore

--- assignment3/gan_training.py
+++ assignment3/gan_training.py
@@ -15,68 +15,8 @@ if T.cuda.is_available():
 else:
     device = "cpu"
 
-
 from discrim import wgan_objective, Generator, ConvDiscriminator
-# DIM = 128
 
-
-# class Generator(nn.Module):
-#     def __init__(self):
-#         super(Generator, self).__init__()
-#         preprocess = nn.Sequential(
-#             nn.Linear(100, 4 * 4 * 4 * DIM),
-#         )
-#
-#         block1 = nn.Sequential(
-#             nn.BatchNorm2d(4 * DIM),
-#             nn.ReLU(True),
-#             nn.ConvTranspose2d(4 * DIM, 2 * DIM, 2, stride=2),
-#             nn.BatchNorm2d(2 * DIM),
-#             nn.ReLU(True),
-#         )
-#         block2 = nn.Sequential(
-#             nn.ConvTranspose2d(2 * DIM, DIM, 2, stride=2),
-#             nn.BatchNorm2d(DIM),
-#             nn.ReLU(True),
-#         )
-#         deconv_out = nn.ConvTranspose2d(DIM, 3, 2, stride=2)
-#
-#         self.preprocess = preprocess
-#         self.block1 = block1
-#         self.block2 = block2
-#         self.deconv_out = deconv_out
-#         self.sigmoid = nn.Sigmoid()
-#
-#     def forward(self, input):
-#         output = self.preprocess(input)
-#         output = output.view(-1, 4 * DIM, 4, 4)
-#         output = self.block1(output)
-#         output = self.block2(output)
-#         output = self.deconv_out(output)
-#         output = self.sigmoid(output)
-#         return output.view(-1, 3, 32, 32)
-#
-#
-# class Discriminator(nn.Module):
-#     def __init__(self):
-#         super(Discriminator, self).__init__()
-#         main = nn.Sequential(
-#             nn.Conv2d(3, DIM, 3, 2, padding=1),
-#             nn.LeakyReLU(),
-#             nn.Conv2d(DIM, 2 * DIM, 3, 2, padding=1),
-#             nn.LeakyReLU(),
-#             nn.Conv2d(2 * DIM, 4 * DIM, 3, 2, padding=1),
-#             nn.LeakyReLU(),
-#         )
-#
-#         self.main = main
-#         self.linear = nn.Linear(4*4*4*DIM, 1)
-#
-#     def forward(self, input):
-#         output = self.main(input)
-#         output = output.view(-1, 4*4*4*DIM)
-#         output = self.linear(output)
-#         return output
 
 class GAN(nn.Module):
     def __init__(self, zdim=100, channels_g=32, channels_d=32, blocks=[1, 1, 1, 1, 1]):
