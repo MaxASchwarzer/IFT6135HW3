Bug fix shape for log-likelihood calculation

assignment3/vae_svhn_model.py
@@ -493,10 +493,12 @@ class vaeSVHNModel(nn.Module) :
 			# Get the i-th datapoint in proper shape
 			x_base = x_input[i]
 			x_batch = np.array([x_base for _ in range(1)]).astype(np.float32) # No need to perform multiple things now
-			# Reshape to image
-			x_batch = np.reshape(x_batch, [-1, 32, 32, 3])
+			# # Reshape to image
+			# x_batch = np.reshape(x_batch, [-1, 32, 32, 3])
 			# Convert into a tensor
 			x_tensor = torch.Tensor(x_batch)
+			# # Make first dimension as channel
+			# x_tensor = x_tensor.permute(0, 3, 1, 2)
 			# Load to device
 			if 'cuda' in self.device :
 				x = x_tensor.cuda()